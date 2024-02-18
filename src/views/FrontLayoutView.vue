10bdaabbfafad026ca53384dc4c8e8e7ea4f725c
yuling <gb2013429@gmail.com>
2024-02-19 00:16:18 +0800

    update 成元件

---
+++ src/views/FrontLayoutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <front-layout-header />
+  <router-view />
+  <front-layout-footer />
+</template>
+
+<script>
+import FrontLayoutHeader from '../components/FrontLayoutHeader.vue';
+import FrontLayoutFooter from '../components/FrontLayoutFooter.vue';
+export default {
+  components: {
+    FrontLayoutHeader,FrontLayoutFooter
+  },
+  // 其他相關設定...
+};
+</script>
