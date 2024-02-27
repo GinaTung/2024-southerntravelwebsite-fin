b3002c888c57ebb6df33a3d9d551ae0c3f16e9f9
yuling <gb2013429@gmail.com>
2024-02-27 12:34:57 +0800

    deploy

--- assets/TouristBudgetView-ak4FSPmZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as s,o as t,a as c}from"./index-5tMyJ4J4.js";const o={},a={class:"container"},n=c("h1",{class:"mb-30"},"This is an 南部旅遊預算 page",-1),_=[n];function r(i,d){return t(),s("div",a,_)}const h=e(o,[["render",r]]);export{h as default};
