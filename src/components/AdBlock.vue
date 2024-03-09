38e3f5d6c8957ce32edfd1477c1d718a14ac0c21
yuling <gb2013429@gmail.com>
2024-03-09 20:09:56 +0800

    update

--- src/components/AdBlock.vue
+++ src/components/AdBlock.vue
@@ -2,14 +2,14 @@
     <div class="ad">
     <div class="container py-10 py-lg-30 d-flex flex-lg-row flex-column justify-content-lg-between text-center text-lg-start">
       <div class="ad-l-side mb-5 mb-lg-0">
-        <p class="fs-4 fs-lg-1 fw-bold mb-5 mb-lg-6">計算荷包 盡情遊覽</p>
-        <p class="fs-4 fs-lg-1 mb-8 fw-bold">我們的旅程 為您量身定制！</p>
-        <p class="fs-6 fs-lg-4 mb-2">Budget Smart, Travel Big</p>
-        <p class="fs-6 fs-lg-4">Your Custom Journey Awaits.</p>
+        <p class="fs-4 text-center fs-lg-1 fw-bold mb-5 mb-lg-6">計算荷包 盡情遊覽</p>
+        <p class="fs-4 text-center fs-lg-1 mb-8 fw-bold">我們的旅程 為您量身定制！</p>
+        <p class="fs-6 text-center fs-lg-4 mb-2">Budget Smart, Travel Big</p>
+        <p class="fs-6 text-center fs-lg-4">Your Custom Journey Awaits.</p>
       </div>
       <div class="ad-r-side d-flex flex-column align-items-lg-end justify-content-lg-center align-items-center ">
-        <a href="tourist-budget.html" class="btn-cerulean-blue btn-cerulean-blue-spacer mb-2 mb-lg-4 d-inline-block fs-5" type="button">立即開始</a>
-        <p class="fs-7 fs-lg-6 fw-bold">根據天數與人數，推薦合適行程</p>
+        <a href="https://ginatung.github.io/2024-southerntravelwebsite-fin/#/TouristBudget" class="btn-cerulean-blue btn-cerulean-blue-spacer mb-2 mb-lg-4 d-inline-block fs-5" type="button">立即開始</a>
+        <p class="fs-7 fs-lg-6 fw-bold text-center">根據天數與人數，推薦合適行程</p>
       </div>
     </div>
     
