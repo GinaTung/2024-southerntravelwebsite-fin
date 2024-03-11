7a11e7004c1f766e6736e7262e73f2e16ee9ae2d
yuling <gb2013429@gmail.com>
2024-03-11 23:15:58 +0800

    deploy

--- assets/collapse-CkUJBuLw.js
+++ assets/collapse-CkUJBuLw.js
@@ -1,4 +1,4 @@
-import{A as M,B as nt}from"./index-vzW9V7O9.js";var Z={exports:{}},K={exports:{}},V={exports:{}};/*!
+import{A as M,B as nt}from"./index-imgO6JzM.js";var Z={exports:{}},K={exports:{}},V={exports:{}};/*!
   * Bootstrap data.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
