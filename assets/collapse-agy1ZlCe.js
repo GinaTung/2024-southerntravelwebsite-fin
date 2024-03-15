a95507b7f3dbe8be22c8d9092d0a7d98ad77419b
yuling <gb2013429@gmail.com>
2024-03-15 19:05:35 +0800

    deploy

--- assets/collapse-agy1ZlCe.js
+++ assets/collapse-agy1ZlCe.js
@@ -1,4 +1,4 @@
-import{A as M,B as nt}from"./index-imgO6JzM.js";var Z={exports:{}},K={exports:{}},V={exports:{}};/*!
+import{D as M,E as nt}from"./index-OzyLONWC.js";var Z={exports:{}},K={exports:{}},V={exports:{}};/*!
   * Bootstrap data.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
