39a0da079f953ad4ea82e997a4308d3390d0c4ad
yuling <gb2013429@gmail.com>
2024-04-08 00:43:30 +0800

    deploy

--- assets/collapse-z81EOXWM.js
+++ assets/collapse-z81EOXWM.js
@@ -1,4 +1,4 @@
-import{C as M,D as nt}from"./index-ugEbYFOy.js";var Z={exports:{}},K={exports:{}},V={exports:{}};/*!
+import{C as M,D as nt}from"./index-Rq_wC_Uc.js";var Z={exports:{}},K={exports:{}},V={exports:{}};/*!
   * Bootstrap data.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
