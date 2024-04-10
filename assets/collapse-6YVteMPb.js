ea850cc1e44f0d055b8aa13822a43f5b2d0ffd32
yuling <gb2013429@gmail.com>
2024-04-10 19:12:03 +0800

    deploy

--- assets/collapse-6YVteMPb.js
+++ assets/collapse-6YVteMPb.js
@@ -1,4 +1,4 @@
-import{E as M,G as nt}from"./index-COVl-sUH.js";var Z={exports:{}},K={exports:{}},V={exports:{}};/*!
+import{E as M,G as nt}from"./index-vL5Jgdvl.js";var Z={exports:{}},K={exports:{}},V={exports:{}};/*!
   * Bootstrap data.js v5.3.2 (https://getbootstrap.com/)
   * Copyright 2011-2023 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
