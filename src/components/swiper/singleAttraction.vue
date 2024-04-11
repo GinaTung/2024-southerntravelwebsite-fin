9acd7f989d8b47b5edf4cf7b0e4db0dd87138857
yuling <gb2013429@gmail.com>
2024-04-11 13:29:17 +0800

    add img放大

--- src/components/swiper/singleAttraction.vue
+++ src/components/swiper/singleAttraction.vue
@@ -11,6 +11,12 @@
   >
     <swiper-slide v-for="(item, key) in imagesUrl" :key="key + 123">
       <img :src="item" class="img-fluid" />
+      <button
+        class="position-absolute text-white end-5 bottom-5 fs-4 bg-transparent border-0"
+        @click="openModal(item, key)"
+      >
+        <i class="bi bi-zoom-in"></i>
+      </button>
     </swiper-slide>
     <button class="swiper-button-next border-0 bg-transparent"></button>
     <button class="swiper-button-prev border-0 bg-transparent"></button>
@@ -28,6 +34,30 @@
       <img :src="item" class="img-fluid" />
     </swiper-slide>
   </swiper>
+  <!-- 放大图片的模态框 -->
+  <!-- <div v-if="showModal" class="modal" @click="closeModal()">
+    <img :src="modalImageFilePath" class="modal-content" />
+  </div> -->
+  <!-- bigModal -->
+  <div
+    class="modal fade"
+    id="bigModal"
+    tabindex="-1"
+    aria-labelledby="bigModalLabel"
+    aria-hidden="true"
+    ref="bigModal"
+  >
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+      <div class="modal-content">
+        <div class="modal-body">
+          <img :src="modalImageFilePath" class="modal-content" />
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import { ref } from 'vue'
@@ -45,6 +75,7 @@ import '@/scss/components/_singleAttraction.scss'
 
 // import required modules
 import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
+import Modal from 'bootstrap/js/dist/modal'
 const api_url2 = import.meta.env.VITE_API_URL2
 import sweetAlert from '@/js/sweetAlert'
 
@@ -58,10 +89,21 @@ export default {
       attractionTitle: '',
       attractions: [],
       enabledAttractions: [],
-      imagesUrl: ''
+      imagesUrl: '',
+      showModal: false,
+      modalImageFilePath: '', // 存储被点击图片的文件路径
+      modalImageIndex: 0, // 存储被点击图片的索引
+      bigModal: null
     }
   },
   methods: {
+    openModal(filePath, index) {
+      this.bigModal.show()
+      this.modalImageFilePath = filePath
+      this.modalImageIndex = index
+      this.showModal = true
+      console.log(filePath, index)
+    },
     getAttractions() {
       this.axios
         .get(`${api_url2}/attractions`)
@@ -93,6 +135,10 @@ export default {
     }
   },
   mounted() {
+    this.bigModal = new Modal(this.$refs.bigModal, {
+      keyboard: false,
+      backdrop: 'static'
+    })
     this.attractionTitle = this.$route.params.title
     this.getAttractions()
   }
