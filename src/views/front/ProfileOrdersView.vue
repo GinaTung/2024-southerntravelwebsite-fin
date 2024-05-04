f7f93348a0ab1e6ba57556eb78a0e0c902380f93
yuling <gb2013429@gmail.com>
2024-05-04 23:27:22 +0800

    update

--- src/views/front/ProfileOrdersView.vue
+++ src/views/front/ProfileOrdersView.vue
@@ -11,16 +11,29 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(item, index) in orders" :key="index + 123">
-        <th scope="row">{{ index + 1 }}</th>
-        <td>{{ item.user.create_at + formatId(item.id) }}</td>
-        <td>Otto</td>
-        <td>${{ finalTotal }}</td>
-        <td>@mdo</td>
-        <td>
-            <button class="btn" type="button">查看詳情</button>
-        </td>
-      </tr>
+      <template v-for="(item, index) in orders" :key="index + 123">
+        <tr>
+          <th scope="row" class="align-middle text-center">{{ index + 1 }}</th>
+          <td class="align-middle">{{  getNewOrderNum(item) }}</td>
+          <td class="align-middle">{{ getOrderDate(item) }}</td>
+          <td class="align-middle">$ {{ item.cartsData[0].final_total }}</td>
+          <td>
+            item.status:{{ item.status }}<br />
+            item.billStatus:{{ item.billStatus }}<br />
+            item.checkDataStatus:{{ item.checkDataStatus }}<br />
+            item.user.status:{{ item.user.status }}<br />
+          </td>
+          <td class="align-middle">
+            <button class="btn" type="button" @click="showDetails(index)">查看詳情</button>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="6" v-if="selectedOrder === index">
+            訂單編號：{{  getNewOrderNum(item) }}<br />
+            其他訂單資訊...
+          </td>
+        </tr>
+      </template>
     </tbody>
   </table>
 </template>
@@ -35,11 +48,39 @@ export default {
       userId: '',
       token: '',
       orders: [],
-      currentDate: '',
-      finalTotal:''
+      orderNum: '',
+      orderDate: '',
+      finalTotal: '',
+      isOpen: false,
+      selectedOrder: null
     }
   },
   methods: {
+    showDetails(index) {
+      console.log(index)
+      this.selectedOrder = index
+    },
+    getNewOrderNum(item){
+    // 獲取日期部分
+    var date = new Date(item.user.create_at);
+    var utcDateTimeString = date.toISOString();
+    var datePart = utcDateTimeString.slice(0, 10);
+    // 格式化日期，移除所有 "-"
+    var formattedDate = datePart.replace(/-/g, '');
+    // 返回組合後的訂單編號
+    return formattedDate + this.formatId(item.id);
+    },
+    getOrderNum(item) {
+      // 這裡返回單個訂單的訂單編號
+      return this.formatId(item.id)
+    },
+    getOrderDate(item) {
+      // 這裡返回單個訂單的日期
+      var date = new Date(item.user.create_at)
+      var utcDateTimeString = date.toISOString()
+      var datePart = utcDateTimeString.slice(0, 10)
+      return datePart
+    },
     formatId(id) {
       // 將id轉化為字符串，檢查長度並根據長度前面填充零
       return id.toString().padStart(3, '0')
@@ -49,24 +90,10 @@ export default {
         .get(`${api_url2}/orders?_embed=cartsData`)
         .then((res) => {
           this.orders = res.data.filter((item) => item.user.userId === this.userId)
-          this.orders.forEach((item) => {
-            console.log(item.user.create_at)
-            var date = new Date(item.user.create_at)
-            var utcDateTimeString = date.toISOString()
-            var datePart = utcDateTimeString.slice(0, 10)
-
-            // 使用 replace() 方法移除所有的 "-" 字符
-            var formattedDate = datePart.replace(/-/g, '')
-
-            // 將轉換後的值賦給原來的 create_at 屬性
-            item.user.create_at = formattedDate
-            console.log(item.cartsData[0].final_total);
-            this.finalTotal = item.cartsData[0].final_total
-          })
-          console.log(this.orders)
         })
         .catch((err) => {
-          sweetAlert.threeLayerCheckType('error', `取得愛心收藏資料失敗`)
+          // sweetAlert.threeLayerCheckType('error', `取得愛心收藏資料失敗`)
+          console.log(err)
         })
     },
     getCookie(cookieName) {
